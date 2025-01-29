b78c561bfed6d78359ebd2c252eefdcba4c08cf3
Alan Rocha <alanrochaarg2001@gmail.com>
2025-01-29 18:53:27 -0300

    Exibindo imagem com Glide

--- app/src/main/java/com/salvatoreacademy/sa_android_nativo_iniciante_app1/CreatureListAdapter.kt
+++ app/src/main/java/com/salvatoreacademy/sa_android_nativo_iniciante_app1/CreatureListAdapter.kt
@@ -4,21 +4,29 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class CreatureListAdapter(private val items: List<Creature>) : RecyclerView.Adapter<CreatureListAdapter.CreatureViewHolder>() {
 
     class CreatureViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindView(item: Creature) {
+
+            // Exbir Numero
             val tvNumber = itemView.findViewById<TextView>(R.id.tvNumber)
             tvNumber.text = item.number.toString()
 
+            // Exibir Nome
             val tvName = itemView.findViewById<TextView>(R.id.tvName)
             tvName.text = item.name
 
+            //Carregamento da imagem com Glide
+            val ivCreature = itemView.findViewById<ImageView>(R.id.ivCreature)
+            Glide.with(itemView).load(item.imageUrl).into(ivCreature)
 
             // Buscar elemento pelo ID e armazenar em variável (val ou var)
         val cvCreature = itemView.findViewById<CardView>(R.id.cvCreature)
